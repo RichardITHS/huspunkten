233d2433d8f971576eba545165ff5e9b8f5b67c1
RichardITHS <110540086+RichardITHS@users.noreply.github.com>
2022-09-08 18:12:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+//Show Navbar
+//targeting toggle-botton and the first array (0)
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+//targeting navbar-links and the first array (0)
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+//Listen on click on toggle button then activate the navbarlinks list
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+  navbarLinks.classList.toggle('active')
 })
+
+//Slideshow 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}   
